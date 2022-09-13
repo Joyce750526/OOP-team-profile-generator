6643d28d5428072f1197b56c65cbaf08580e1462
Joyce Chen <joyceideas@outlook.com>
2022-09-13 19:30:04 -0400

    added class and method in js file

---
+++ index.js
@@ -0,0 +1,38 @@
+// Packages
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const generateHTML = require('./src/generateHtmlPage');
+// const Manager = require('./lib/Manager');
+// const Intern = require('./lib/Intern');
+// const Engineer = require('./lib/Engineer');
+
+
+function startApp (){
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "What is the team manager's name?",
+            },
+            {
+              type: "input",
+              name: "ID",
+              message: "What is the team manager's employee ID?",
+            },
+            {
+              type: "input",
+              name: "email",
+              message: "What is the team manager's email address?",
+            },
+            {
+              type: "input",
+              name: "number",
+              message: "What is the uteam manager's office number?",
+            },
+          ])
+          .then((response) => {
+            console.log(response);
+          });
+      }
+      startApp () // calling a function
